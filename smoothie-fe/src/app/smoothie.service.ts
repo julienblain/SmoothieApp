7bbfe6efd8f0c1f426044776a2eac312d821c8a6
julblain <blain.julien111@gmail.com>
2019-07-11 10:46:37 +0200

    create service smoothie update

--- smoothie-fe/src/app/smoothie.service.ts
+++ smoothie-fe/src/app/smoothie.service.ts
@@ -1,11 +1,45 @@
 import { Injectable } from '@angular/core';
 import {environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
+export interface Smoothie {
+  _id: string;
+  title: string;
+  ingredients: [
+    {
+      nom: string;
+      quantité: string;
+    }];
+  features: {
+    eTcost: string;
+    preparime: string;
+  };
+  advice: string;
+  description: string;
+  steps: [
+    {
+        stepText: string;
+    }
+  ];
+  photo:[
+    {
+      titre: string;
+      path: string;
+      description: string;
+    }
+  ];
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SmoothieService {
+
   private apiUtl = environment.apiUrl;
-  constructor() { }
+
+  constructor(
+    private htpp:HttpClient,
+    private router:Router
+  ) {}
 }
